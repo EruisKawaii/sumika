94d0b6630b53c76e133c301cfc10aaf144fcf2cc
EruisKawaii <63264196+EruisKawaii@users.noreply.github.com>
2024-06-02 18:37:43 -0700

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,101 +1,101 @@
 export const animeEndings = [
   {
-    name: "I Want To Eat Your Pancreas	Opening 1	Fanfare by sumika",
+    name: "I Want To Eat Your Pancreas - Fanfare by sumika",
     videoId: "https://ladist1.catbox.video/66vyxz.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 1"
+        "OP"
       ]
     }
   },
   {
-    name: "I Want To Eat Your Pancreas	Ending 1	Shunkashuutou by sumika",
+    name: "I Want To Eat Your Pancreas - Shunkashuutou by sumika",
     videoId: "https://ladist1.catbox.video/7qtkj3.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Ending 1"
+        "ED"
       ]
     }
   },
   {
-    name: "I Want To Eat Your Pancreas	Insert Song	Himitsu by sumika",
+    name: "I Want To Eat Your Pancreas - Himitsu by sumika",
     videoId: "https://ladist1.catbox.video/gaawy8.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Insert Song"
+        "INS"
       ]
     }
   },
   {
-    name: "Wotakoi: Love is Hard for Otaku	Opening 1	Fiction by sumika",
+    name: "Wotakoi: Love is Hard for Otaku - Fiction by sumika",
     videoId: "https://ladist1.catbox.video/l37nxf.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 1"
+        "OP"
       ]
     }
   },
   {
-    name: "Mix	Opening 1	Equal by sumika",
+    name: "Mix - Equal by sumika",
     videoId: "https://ladist1.catbox.video/v2zl6w.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 1"
+        "OP"
       ]
     }
   },
   {
-    name: "My Hero Academia: Heroes: Rising	Ending 1	Higher Ground by sumika",
+    name: "My Hero Academia: Heroes: Rising - Higher Ground by sumika",
     videoId: "https://ladist1.catbox.video/drsio5.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Ending 1"
+        "ED"
       ]
     }
   },
   {
-    name: "Pretty Boy Detective Club	Opening 1	Shake & Shake by sumika",
+    name: "Pretty Boy Detective Club - Shake & Shake by sumika",
     videoId: "https://ladist1.catbox.video/h3ynhd.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 1"
-            ]
+        "OP"
+      ]
     }
   },
-{
-    name: "A Couple of Cuckoos	Opening 2	Glitter by sumika",
+  {
+    name: "A Couple of Cuckoos - Glitter by sumika",
     videoId: "https://vhdist1.catbox.video/epmhvh.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 2"
+        "OP"
       ]
     }
   },
-{
-    name: "Mix: Meisei Story Season 2	Opening 1	Starting Over by sumika",
+  {
+    name: "Mix: Meisei Story Season 2 - Starting Over by sumika",
     videoId: "https://ladist1.catbox.video/tg3l3h.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 1"
+        "OP"
       ]
     }
   },
-{
-    name: "Delicious in Dungeon	Opening 2	Unmei by sumika",
+  {
+    name: "Delicious in Dungeon - Unmei by sumika",
     videoId: "https://ladist1.catbox.video/3he1jg.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "Opening 2"
+        "OP"
       ]
     }
   }
